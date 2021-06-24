87f439a27caefc1d1c8f2d142944471dce151934
unknown <cmwillett87@gmail.com>
2021-06-24 14:09:35 -0400

    created API views in views.py

--- comments_API/comments/views.py
+++ comments_API/comments/views.py
@@ -1,3 +1,138 @@
-from django.shortcuts import render
+from .models import Comment, Reply
+from .serializers import CommentSerializer, ReplySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+
 
 # Create your views here.
+class ReplyList(APIView):
+
+    def get(self, request):
+        reply = Reply.objects.all()
+        serializer = ReplySerializer(reply, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ReplyDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Reply.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        reply = self.get_object(pk)
+        serializer = ReplySerializer(reply)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        reply = self.get_object(pk)
+        serializer = ReplySerializer(reply, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        reply = self.get_object(pk)
+        reply.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentList(APIView):
+
+    def get(self, request):
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        comment = self.get_object(pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentLike(APIView):
+
+    def get_object(self, pk, video_id):
+        try:
+            return Comment.objects.get(pk=pk, video_id=video_id)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    def patch(self, request, pk, video_id):
+        comment = self.get_object(pk, video_id=video_id)
+        data = {"likes": comment.likes + int(1)}
+        serializer = CommentSerializer(comment, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDislike(APIView):
+
+    def get_object(self, pk, likes):
+        try:
+            return Comment.objects.get(pk=pk, likes=likes)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    def patch(self, request, pk, likes):
+        comment = self.get_object(pk, likes=likes)
+        data = {"dislikes": comment.dislikes + int(1)}
+        serializer = CommentSerializer(comment, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
