1962003fe9eb3dfdc42f11c538ebf58a286104cb
unknown <cmwillett87@gmail.com>
2021-06-24 13:30:52 -0400

    created a serializers.py with Comment and Reply serializer models with fields.

---
+++ comments_API/comments/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Comment, Reply
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['video_id', 'body', 'likes', 'dislikes']
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['body']
