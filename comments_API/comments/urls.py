d80045f0df18bd8324082d1cdf03d9c8ea73cd5b
unknown <cmwillett87@gmail.com>
2021-06-28 09:49:17 -0400

    Updated the comments detail view and url path

--- comments_API/comments/urls.py
+++ comments_API/comments/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('comments/', views.CommentList.as_view()),
-    path('comments/<int:pk>/', views.CommentDetail.as_view()),
+    path('comments-id/<str:video_id>/', views.CommentDetail.as_view()),
     path('comments/<int:pk>/<str:video_id>/', views.CommentLike.as_view()),
     path('comments/<str:video_id>/<int:pk>/', views.CommentDislike.as_view()),
     path('comments-reply/', views.ReplyList.as_view()),
