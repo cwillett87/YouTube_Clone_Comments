7432ab347d61b3d5f89c9004bd6eaab1c83ac30b
Jeremiah Booth <jereimah.boothphotog@gmail.com>
2021-06-28 13:37:28 -0700

    fixed field

--- comments_API/comments/serializers.py
+++ comments_API/comments/serializers.py
@@ -5,7 +5,7 @@ from .models import Comment, Reply
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['video_id', 'body', 'likes', 'dislikes']
+        fields = ['id','video_id', 'body', 'likes', 'dislikes']
 
 
 class ReplySerializer(serializers.ModelSerializer):
